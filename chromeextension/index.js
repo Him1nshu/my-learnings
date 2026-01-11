8ab7af5444781d8691b9fad9cadb4fd0f3fe61d3
Him1nshu <astng888@gmail.com>
2026-01-11 23:15:05 +0530

    changes to js

--- chromeextension/index.js
+++ chromeextension/index.js
@@ -5,20 +5,14 @@ const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
-
-function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
+    ulEl.innerHTML += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='inputEl.value' >
+                    ${inputEl.value}
                 </a>
             </li>
         `
-    }
-    ulEl.innerHTML = listItems  
-}
+    inputEl.value = ""
+})
+
+
